1ab7abe9b81ceeb13eb734efc6267347966c8499
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2017-07-23 09:36:59 +0200

    Basic write, update and delete database operations

--- app/src/main/java/dpyl/eddy/piedfly/Database.java
+++ app/src/main/java/dpyl/eddy/piedfly/Database.java
@@ -1,54 +1,96 @@
 package dpyl.eddy.piedfly;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import dpyl.eddy.piedfly.model.Flock;
 import dpyl.eddy.piedfly.model.User;
 
 public class Database {
 
-    private Context context;
-    private FirebaseDatabase mDatabase;
-
-    private Database(Context context){
-        this.context = context;
-        mDatabase = FirebaseDatabase.getInstance();
+    public static void createUser(@NonNull User user) {
+        if(user.getUid() == null)
+            throw new RuntimeException("User has no uid");
+        else {
+            final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+            userRef.setValue(user);
+        }
     }
 
-    public static Database init(Context context){
-        return new Database(context);
+    public static void deleteUser(@NonNull String uid) {
+        final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(uid);
+        userRef.removeValue();
     }
 
-    /**
-     * Updates the user's data but, if the user already exists in the database, the {@code User.flocks} is ignored.
-     * @param user The user that is to be written, {@code User.flocks} may be ignored.
-     */
-    public void writeUser(final User user){
-        final DatabaseReference userRef = mDatabase.getReference("users").child(user.getUid());
-        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    User oldUser = dataSnapshot.getValue(User.class);
-                    if(oldUser != null){
-                        Map<String, Boolean> flocks = oldUser.getFlocks();
-                        user.setFlocks(flocks);
-                    }
-                } userRef.setValue(user);
-            }
+    public static void updateUser(@NonNull User user) {
+        if(user.getUid() == null)
+            throw new RuntimeException("User has no uid");
+        else {
+            final DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+            final Map<String, Object> childUpdates = new HashMap<>();
+            if(user.getDisplayName() != null)
+                childUpdates.put("/displayName", user.getDisplayName());
+            if(user.getPhotoUrl() != null)
+                childUpdates.put("/photoUrl", user.getPhotoUrl());
+            if(user.getCountryISO() != null)
+                childUpdates.put("/countryISO", user.getCountryISO());
+            if(user.getLastKnownLocation() != null)
+                childUpdates.put("/lastKnownLocation", user.getLastKnownLocation());
+            if(user.getFlocks() != null)
+                childUpdates.put("/flocks", user.getFlocks());
+            userRef.updateChildren(childUpdates);
+        }
+    }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                // TODO: Error handling
-            }
-        });
+    public static void createFlock(@NonNull Flock flock) {
+        if(flock.getName() == null)
+            throw new RuntimeException("Flock has no name");
+        else {
+            final DatabaseReference flockRef = FirebaseDatabase.getInstance().getReference("flocks").push();
+            flock.setKey(flockRef.getKey());
+            flockRef.setValue(flock);
+        }
+    }
+
+    public static void deleteFlock(@NonNull String key) {
+        final DatabaseReference flockRef = FirebaseDatabase.getInstance().getReference("flocks").child(key);
+        flockRef.removeValue();
+    }
+
+    public static void updateFlock(@NonNull Flock flock) {
+        if(flock.getKey() == null)
+            throw new RuntimeException("Flock has no key");
+        else {
+            DatabaseReference flockRef = FirebaseDatabase.getInstance().getReference("flocks").child(flock.getKey());
+            final Map<String, Object> childUpdates = new HashMap<>();
+            if(flock.getName() != null)
+                childUpdates.put("/name", flock.getName());
+            if(flock.getUsers() != null)
+                childUpdates.put("/users", flock.getUsers());
+            flockRef.updateChildren(childUpdates);
+        }
+    }
+
+    public static void addUserToFlock(@NonNull String uid, @NonNull String key) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference userRef = database.getReference("users").child(uid).child("flocks").child(key);
+        final DatabaseReference flockRef = database.getReference("flocks").child(key).child("users").child(uid);
+        userRef.setValue(true);
+        flockRef.setValue(true);
+    }
+
+    public static void removeUserFromFlock(@NonNull String uid, @NonNull String key) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference userRef = database.getReference("users").child(uid).child("flocks").child(key);
+        final DatabaseReference flockRef = database.getReference("flocks").child(key).child("users").child(uid);
+        userRef.removeValue();
+        flockRef.removeValue();
     }
 
 }
--- app/src/main/java/dpyl/eddy/piedfly/view/MainActivity.java
+++ app/src/main/java/dpyl/eddy/piedfly/view/MainActivity.java
@@ -2,12 +2,7 @@ package dpyl.eddy.piedfly.view;
 
 import android.os.Bundle;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import dpyl.eddy.piedfly.Database;
 import dpyl.eddy.piedfly.R;
-import dpyl.eddy.piedfly.model.User;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,18 +10,5 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // TODO: Used for live testing the database, delete on production
-        if(auth != null && auth.getCurrentUser() != null){
-            Map<String, Boolean> flocks = new HashMap<>();
-            flocks.put("123", true);
-            flocks.put("456", true);
-            User user = new User(auth.getCurrentUser().getUid(), "pepe", "", "ES", "123", flocks);
-            Database.init(this).writeUser(user);
-        }
     }
 }
